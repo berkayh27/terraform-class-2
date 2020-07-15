7239aecfda0419f72a48d6b5d7c46c6592865f41
berkayh27 <berkayhidiroglu@hotmail.com>
2020-07-15 21:36:07 +0000

    version

--- class2/backend.tf
+++ class2/backend.tf
@@ -1,8 +1,15 @@
 terraform {
+
+required_version = "0.11.14"
+required_providers {
+  aws = "2.68.0"
+  }
+
 backend "s3" {
-bucket = "terraform-state-class-berkay"
-key = "jenkins/us-east-1/tools/virgina/jenkins.tfstate"
-region = "us-east-1"
-# dynamodb_table = "prod"
+  bucket = "terraform-state-class-berkay"
+  key = "jenkins/us-east-1/tools/virgina/jenkins.tfstate"
+  region = "us-east-1"
+  # dynamodb_table = "prod"
+
   }
 }
